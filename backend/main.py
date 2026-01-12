53d1158b133347e8e0a89c0a6a1376f12531eb4d
Dharmik Modi <dharmik.turabit@gmail.com>
2026-01-12 12:51:20 +0530

    feat: backend logic and SQL queries for employee CRUD Operation

--- backend/main.py
+++ backend/main.py
@@ -9,23 +9,29 @@ class Login(BaseModel):
     email: str
     password: str
 
-class AgentRegister(BaseModel):
-    team_leader_email : str
-    name : str
-    email : str
-    mobile_number : str
-    password : str
+# class AgentRegister(BaseModel):
+#     team_leader_email : str
+#     name : str
+#     email : str
+#     mobile_number : str
+#     password : str
 
-class ServicePersonRegister(BaseModel):
-    agent_email : str
+# class ServicePersonRegister(BaseModel):
+#     agent_email : str
+#     name : str
+#     email : str 
+#     mobile_number : str 
+#     password : str 
+#     type : str 
+#     city : str 
+#     state : str 
+#     country : str
+class EmployeeRegister(BaseModel):
     name : str
     email : str 
     mobile_number : str 
     password : str 
-    type : str 
-    city : str 
-    state : str 
-    country : str
+    type : str
 
 class CustomerRegister(BaseModel):
     agent_email : str
@@ -350,6 +356,113 @@ app = FastAPI()
 #                     )    
 #     except Exception as e:
 #         return e
+
+# -------------------------------------- Employee ----------------------------------------------
+@app.post("/employee_registration")
+def employee_registration(data:EmployeeRegister,db = Depends(access_db)):
+    try:
+        with db:
+            with db.cursor() as cursor:
+                cursor.execute("select * from employee where employee_email = %s",(data.email,))
+                d = cursor.fetchone()
+                if d:
+                    raise HTTPException(
+                        status_code=status.HTTP_409_CONFLICT,
+                        detail="Email already exist"
+                    )
+                cursor.execute("select employee_type_id from employee_type where type_name=%s",(data.type,))
+                type_id = cursor.fetchone()["employee_type_id"]
+                query = '''
+                        insert into employee(
+                        employee_name, 
+                        employee_email, 
+                        employee_mobile_number, 
+                        employee_password, 
+                        employee_type
+                        ) values (%s,%s,%s,%s,%s)
+                    '''
+                values = (data.name,data.email,data.mobile_number,data.password,type_id)
+                cursor.execute(query,values)
+                db.commit()
+                return {"status_code":status.HTTP_201_CREATED, "message":"Employee registered"}
+    except Exception as e:
+        return e
+
+@app.get("/all_employees")
+def fetch_all_employees(db = Depends(access_db)):
+    with db:
+        with db.cursor() as cursor:
+            employees = cursor.execute("select * from employee")
+            if employees:
+                d = cursor.fetchall()
+                return d
+            else:
+                raise HTTPException(
+                        status_code=status.HTTP_404_NOT_FOUND,
+                        detail="employee not found"
+                    )
+    
+@app.post("/employee_login")
+def employee_login(data: Login,db = Depends(access_db)):
+    try:
+        c = db.cursor()
+        c.execute("select employee_email,employee_password from employee where employee_email = %s",(data.email,))
+        d = c.fetchone()
+        if d is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid email"
+            )
+
+        if data.password != d["employee_password"]:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid password"
+            )
+
+        print("success")
+        return {"message": "Login successful"}
+    except Exception as e:
+        return e
+
+@app.put("/employee_update")
+def update_employee(data : EmployeeRegister,db = Depends(access_db)):
+    try:
+        with db:
+            with db.cursor() as cursor:
+                cursor.execute("select employee_email from employee where employee_email = %s",(data.email,))
+                d = cursor.fetchone()
+                cursor.execute("select employee_type_id from employee_type where type_name=%s",(data.type,))
+                type_id = cursor.fetchone()["employee_type_id"]
+                if d:
+                    cursor.execute("update employee set employee_name=%s,employee_mobile_number=%s,employee_password=%s,employee_type=%s where employee_email=%s",(data.name,data.mobile_number,data.password,type_id,data.email))
+                    db.commit()
+                    return status.HTTP_202_ACCEPTED
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail="Email not exist"
+                )
+    except Exception as e:
+        return e
+    
+@app.delete("/employee_remove")
+def remove_employee(email:str,db = Depends(access_db)):
+    try:
+        with db:
+            with db.cursor() as cursor:
+                cursor.execute("select employee_email from employee where employee_email = %s",(email,))
+                d = cursor.fetchone()
+                if d:
+                    cursor.execute("delete from employee where employee_email = %s",(email))
+                    db.commit()
+                    return status.HTTP_200_OK
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail="Email not exist"
+                )
+    except Exception as e:
+        return e
+
 
 
 # # -------------------------------------- customer ----------------------------------------------
--- sql_queries.sql
+++ sql_queries.sql
@@ -1,37 +1,41 @@
 create database smart_support_desk;
 use smart_support_desk;
 
-create Table team_leader(
-	team_leader_id int auto_increment primary key,
-    team_leader_name varchar(30),
-    team_leader_email varchar(40) unique key,
-    team_leader_mobile_number varchar(10) unique key,
-    team_leader_password varchar(20)
-);
+-- create Table team_leader(
+-- 	team_leader_id int auto_increment primary key,
+--     team_leader_name varchar(30),
+--     team_leader_email varchar(40) unique key,
+--     team_leader_mobile_number varchar(10) unique key,
+--     team_leader_password varchar(20)
+-- );
+-- drop table team_leader;
+-- create table agent(
+-- 	agent_id int auto_increment primary key,
+--     agent_name varchar(30),
+--     agent_email varchar(40) unique key,
+--     agent_mobile_number varchar(10) unique key,
+--     agent_password varchar(20),
+--     agent_ticket_count int,
+--     agent_rating int
+-- );
+-- drop table agent;
+-- create table service_person(
+-- 	service_person_id int auto_increment primary key,
+--     service_person_name varchar(30),
+--     service_person_email varchar(40) unique key,
+--     service_person_mobile_number varchar(10) unique key,
+--     service_person_password varchar(20),
+--     service_person_type varchar(20),
+--     service_person_rating int,
+--     service_person_city varchar(30),
+--     service_person_state varchar(30),
+-- 	service_person_country varchar(30),
+-- 	service_person_address varchar(100)
+-- );
+-- drop table service_person;
 
-create table agent(
-	agent_id int auto_increment primary key,
-    agent_name varchar(30),
-    agent_email varchar(40) unique key,
-    agent_mobile_number varchar(10) unique key,
-    agent_password varchar(20),
-    agent_ticket_count int,
-    agent_rating int
-);
 
-create table service_person(
-	service_person_id int auto_increment primary key,
-    service_person_name varchar(30),
-    service_person_email varchar(40) unique key,
-    service_person_mobile_number varchar(10) unique key,
-    service_person_password varchar(20),
-    service_person_type varchar(20),
-    service_person_rating int,
-    service_person_city varchar(30),
-    service_person_state varchar(30),
-	service_person_country varchar(30),
-	service_person_address varchar(100)
-);
+
 
 create Table customer(
 	customer_id int auto_increment primary key,
@@ -45,6 +49,45 @@ create Table customer(
     customer_address varchar(100)
 );
 
+-- create table ticket(
+-- 	ticket_id int auto_increment primary key,
+--     issue_title varchar(30),
+--     issue_type varchar(20),
+--     issue_description varchar(100),
+--     priority enum("High","Medium","Low"),
+--     reason varchar(50),
+--     generate_datetime datetime,
+--     solve_datetime datetime,
+--     ticket_status enum("Open","In_Progress","Close"),
+--     agent_id int,
+--     customer_id int,
+--     service_person_id int,
+--     foreign key (agent_id) references agent(agent_id),
+--     foreign key (customer_id) references customer(customer_id),
+--     foreign key (service_person_id) references service_person(service_person_id)
+-- );
+-- alter table ticket modify column ticket_status enum("Open","In_Progress","Close");
+-- drop table ticket;
+
+drop table ticket_log;
+
+create table employee_type(
+	employee_type_id int auto_increment primary key,
+    type_name varchar(20)
+    );
+
+create table employee(
+	employee_id int auto_increment primary key,
+    employee_name varchar(30),
+    employee_email varchar(40) unique key,
+    employee_mobile_number varchar(10) unique key,
+    employee_password varchar(20),
+    employee_type int,
+    foreign key (employee_type) references employee_type(employee_type_id)
+	);
+drop table employee;
+
+
 create table ticket(
 	ticket_id int auto_increment primary key,
     issue_title varchar(30),
@@ -54,25 +97,28 @@ create table ticket(
     reason varchar(50),
     generate_datetime datetime,
     solve_datetime datetime,
-    agent_id int,
+    ticket_status enum("Open","In_Progress","Close"),
+    employee_id int,
     customer_id int,
-    service_person_id int,
-    foreign key (agent_id) references agent(agent_id),
     foreign key (customer_id) references customer(customer_id),
-    foreign key (service_person_id) references service_person(service_person_id)
+    foreign key (employee_id) references employee(employee_id)
 );
-alter table ticket modify column ticket_status enum("Open","In_Progress","Close");
+drop table ticket;
 
 create table ticket_log(
 	ticket_log_id int auto_increment primary key,
     ticket_id int,
-    ticket_status enum("Open","In Progress","Close"),
+    ticket_status enum("Open","In_Progress","Close"),
     ticket_log_datetime datetime,
     update_person_type varchar(20),
     update_person_id int,
     foreign key (ticket_id) references ticket(ticket_id)
 );
-alter table ticket_log modify column ticket_status enum("Open","In_Progress","Close");
+drop table ticket_log;
+
+insert into employee_type(employee_type_id,type_name) values (1,"Admin"),(2,"Agent"),(3,"Service Person");
+
+
 
 -- create procedure
 -- create trigger ticket_create_trigger
